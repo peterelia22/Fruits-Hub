317706f154ba6767444d80dcd887b126b9423493
peterelia22 <peterelia914@gmail.com>
2025-06-30 13:24:47 +0300

    Implement SignUpViewBody with input fields for name, email, and password

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/widgets/custom_text_field.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 24,
+            ),
+            CustomTextFormField(
+                hintText: 'الاسم كامل', keyboardType: TextInputType.name),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              keyboardType: TextInputType.emailAddress,
+              hintText: 'البريد الإلكتروني',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFormField(
+              suffixIcon: Icon(
+                Icons.remove_red_eye,
+                color: Color(0xffc9cecf),
+              ),
+              keyboardType: TextInputType.visiblePassword,
+              hintText: 'كلمة المرور',
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
