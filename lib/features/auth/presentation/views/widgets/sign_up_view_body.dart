a5234ebdcc5c1d2a1b2993877843ee3d6177652d
peterelia22 <peterelia914@gmail.com>
2025-07-01 12:20:12 +0300

    Refactor SignUpViewBody to include CustomButton and HaveAnAccount widget, and update padding and const usage

--- lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
+++ lib/features/auth/presentation/views/widgets/sign_up_view_body.dart
@@ -1,34 +1,37 @@
 import 'package:flutter/material.dart';
 import 'package:fruits_hub/constants.dart';
 import 'package:fruits_hub/core/widgets/custom_text_field.dart';
+import 'package:fruits_hub/features/auth/presentation/views/widgets/have_an_account.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/terms_and_conditions.dart';
+
+import '../../../../../core/widgets/custom_button.dart';
 
 class SignUpViewBody extends StatelessWidget {
   const SignUpViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: kHorizontalPadding),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kHorizontalPadding),
       child: SingleChildScrollView(
         child: Column(
           children: [
-            SizedBox(
+            const SizedBox(
               height: 24,
             ),
-            CustomTextFormField(
+            const CustomTextFormField(
                 hintText: 'الاسم كامل', keyboardType: TextInputType.name),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
-            CustomTextFormField(
+            const CustomTextFormField(
               keyboardType: TextInputType.emailAddress,
               hintText: 'البريد الإلكتروني',
             ),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
-            CustomTextFormField(
+            const CustomTextFormField(
               suffixIcon: Icon(
                 Icons.remove_red_eye,
                 color: Color(0xffc9cecf),
@@ -36,10 +39,18 @@ class SignUpViewBody extends StatelessWidget {
               keyboardType: TextInputType.visiblePassword,
               hintText: 'كلمة المرور',
             ),
-            SizedBox(
+            const SizedBox(
               height: 16,
             ),
-            TermsAndConditions()
+            const TermsAndConditions(),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomButton(onPressed: () {}, text: 'إنشاء حساب جديد'),
+            const SizedBox(
+              height: 26,
+            ),
+            const HaveAnAccount()
           ],
         ),
       ),
