31994281dc432ba85cd7c13d8ce68131b339c6d6
peterelia22 <peterelia914@gmail.com>
2025-07-01 13:09:25 +0300

    Integrate Firebase into the project by adding necessary configurations, dependencies, and generated files for Android, iOS, macOS, and Windows platforms.

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:fruits_hub/core/services/shared_preferences_singleton.dart';
@@ -6,9 +7,13 @@ import 'package:fruits_hub/features/splash/presentation/views/splash_view.dart';
 import 'package:fruits_hub/generated/l10n.dart';
 
 import 'core/helper_functions/on_generate_routes.dart';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   await SharedPreferencesSingleton.init();
   runApp(const FruitsHub());
