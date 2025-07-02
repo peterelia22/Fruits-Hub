99cb4f7f2da6b41b4806e4c5c2be1624a282a24a
peterelia22 <peterelia914@gmail.com>
2025-07-02 12:46:51 +0300

    Add flutter_bloc and get_it dependencies; implement GetIt service for dependency injection

--- lib/features/auth/presentation/views/sign_up_view.dart
+++ lib/features/auth/presentation/views/sign_up_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:fruits_hub/core/widgets/custom_app_bar.dart';
+import 'package:fruits_hub/features/auth/presentation/cubits/signup_cubit/signup_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/sign_up_view_body.dart';
 
 class SignUpView extends StatelessWidget {
@@ -7,9 +9,12 @@ class SignUpView extends StatelessWidget {
   static const routeName = 'signUp';
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: buildAppBar(context, title: 'حساب جديد'),
-      body: const SignUpViewBody(),
+    return BlocProvider(
+      create: (context) => SignupCubit(),
+      child: Scaffold(
+        appBar: buildAppBar(context, title: 'حساب جديد'),
+        body: const SignUpViewBody(),
+      ),
     );
   }
 }
