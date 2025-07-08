fd4fb2cb71a2b761670a89b9710fe2eb4d81b60b
peterelia22 <peterelia914@gmail.com>
2025-07-08 11:34:45 +0300

    Implement SigninViewBodyBlocConsumer for improved state management and loading indication; add CustomProgressHud for handling loading states during sign-in.

--- lib/features/auth/presentation/views/signin_view.dart
+++ lib/features/auth/presentation/views/signin_view.dart
@@ -4,9 +4,12 @@ import 'package:fruits_hub/core/services/get_it_service.dart';
 import 'package:fruits_hub/core/utils/app_text_styles.dart';
 import 'package:fruits_hub/features/auth/presentation/cubits/signin_cubit/signin_cubit.dart';
 import 'package:fruits_hub/features/auth/presentation/views/widgets/signin_view_body.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
 
+import '../../../../core/helper_functions/build_error_bar.dart';
 import '../../../../core/widgets/custom_app_bar.dart';
 import '../../domain/repos/auth_repo.dart';
+import 'widgets/signin_view_body_bloc_consumer.dart';
 
 class SigninView extends StatelessWidget {
   const SigninView({super.key});
@@ -17,7 +20,7 @@ class SigninView extends StatelessWidget {
       create: (context) => SigninCubit(getIt<AuthRepo>()),
       child: Scaffold(
         appBar: buildAppBar(context, title: 'تسجيل دخول'),
-        body: const SigninViewBody(),
+        body: const SigninViewBodyBlocConsumer(),
       ),
     );
   }
---
+++ lib/features/auth/presentation/views/widgets/signin_view_body_bloc_consumer.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+import '../../../../../core/helper_functions/build_error_bar.dart';
+import '../../../../../core/widgets/csutom_progress_hud.dart';
+import '../../cubits/signin_cubit/signin_cubit.dart';
+import 'signin_view_body.dart';
+
+class SigninViewBodyBlocConsumer extends StatelessWidget {
+  const SigninViewBodyBlocConsumer({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocConsumer<SigninCubit, SigninState>(
+      listener: (context, state) {
+        if (state is SigninSuccess) {
+          // Handle successful sign-in, e.g., navigate to home screen
+        }
+        if (state is SigninFailure) {
+          buildErrorBar(context, state.message);
+        }
+      },
+      builder: (context, state) {
+        return CustomProgressHud(
+            isLoading: state is SigninLoading ? true : false,
+            child: const SigninViewBody());
+      },
+    );
+  }
+}
