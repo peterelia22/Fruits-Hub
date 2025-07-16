06368d3771e4a730633eaf8b99199e28bcf9a5c3
peterelia22 <peterelia914@gmail.com>
2025-07-16 21:32:25 +0300

    feat: Implement FeaturedList widget for horizontal scrolling of featured items

--- lib/features/home/presentation/views/widgets/featured_item.dart
+++ lib/features/home/presentation/views/widgets/featured_item.dart
@@ -10,55 +10,59 @@ class FeaturedItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var itemWidth = MediaQuery.of(context).size.width;
-    return Container(
-      width: itemWidth,
-      child: AspectRatio(
-        aspectRatio: 342 / 158,
-        child: Stack(
-          children: [
-            Positioned(
-                right: itemWidth * 0.4,
-                bottom: 0,
-                left: 0,
-                top: 0,
-                child: SvgPicture.asset(
-                  Assets.imagesPageView1Image,
-                  fit: BoxFit.fill,
-                )),
-            Container(
-              width: itemWidth * 0.5,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                    image: svg.Svg(Assets.imagesFeaturedItemBackground),
-                    fit: BoxFit.fill),
-              ),
-              child: Padding(
-                padding: const EdgeInsets.only(right: 33),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(
-                      height: 25,
-                    ),
-                    Text('عروض العيد',
-                        style:
-                            TextStyles.regular13.copyWith(color: Colors.white)),
-                    const Spacer(),
-                    Text('خصم 25%',
-                        style: TextStyles.bold19.copyWith(color: Colors.white)),
-                    const SizedBox(
-                      height: 7,
-                    ),
-                    FeaturedItemButton(onPressed: () {}),
-                    const SizedBox(
-                      height: 29,
-                    )
-                  ],
+    var itemWidth = MediaQuery.of(context).size.width - 32;
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(4),
+      child: Container(
+        width: itemWidth,
+        child: AspectRatio(
+          aspectRatio: 342 / 158,
+          child: Stack(
+            children: [
+              Positioned(
+                  right: itemWidth * 0.4,
+                  bottom: 0,
+                  left: 0,
+                  top: 0,
+                  child: SvgPicture.asset(
+                    Assets.imagesPageView1Image,
+                    fit: BoxFit.fill,
+                  )),
+              Container(
+                width: itemWidth * 0.5,
+                decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: svg.Svg(Assets.imagesFeaturedItemBackground),
+                      fit: BoxFit.fill),
                 ),
-              ),
-            )
-          ],
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 33),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const SizedBox(
+                        height: 25,
+                      ),
+                      Text('عروض العيد',
+                          style: TextStyles.regular13
+                              .copyWith(color: Colors.white)),
+                      const Spacer(),
+                      Text('خصم 25%',
+                          style:
+                              TextStyles.bold19.copyWith(color: Colors.white)),
+                      const SizedBox(
+                        height: 7,
+                      ),
+                      FeaturedItemButton(onPressed: () {}),
+                      const SizedBox(
+                        height: 29,
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
